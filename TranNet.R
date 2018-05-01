fed93d2eb1cdd88a629835a6eb8c705295fbd897
Enri <enrico.roletto@carloalberto.org>
2018-05-01 23:34:40 +0200

    Added first try with shortest path.

--- TranNet.R
+++ TranNet.R
@@ -25,23 +25,70 @@ View(speedFlow)
 matOD <- as.data.table(read_excel("Transportmodeling.xlsx", 
                                       sheet = "MAT_OD"))
 View(matOD)
+
+names(network) <- gsub(x = names(network),
+                        pattern = "-",
+                        replacement = "_")
+
+#manage edge list according to directions
+
+#subset two way same ch
+
+network_2way = network[DIR_CODE==0, .(A_NODE=B_NODE, B_NODE=A_NODE, DIST, LINK_TYPE_A_B=LINK_TYPE_B_A, 
+                       CAP_INDEX_A_B=CAP_INDEX_B_A, 
+                       SPEED_A_B=SPEED_B_A, CAPACITY_A_B=CAPACITY_B_A, DIR_CODE)]
+
+network[, c('LINK_TYPE_B_A', 'CAP_INDEX_B_A', 'SPEED_B_A', 'CAPACITY_B_A'):=NULL]
+
+network_complete = rbind(network, network_2way)
 #drop vertices without coordinates
+
+
+#vertex without links
+
+net_edgelist = c(network_complete$A_NODE,network_complete$B_NODE)
+
+coord_complete = coord[(NODE %in% net_edgelist),]
+
+# Load the network
+
+# node 375 is missing from coord table
+# node 375 has links with only two other real point
+cord375E = colMeans(coord[NODE %in% c(643, 2680), .(COORD_X, COORD_Y)])
+
+coord_complete = rbind(coord_complete,
+                data.frame("NODE" = 375, "COORD_X" = cord375E[1], "COORD_Y" = cord375E[2], "NAME" = NA))
+
+#network_complete = merge(network_complete, )
+
+tr_net<-graph_from_data_frame(vertices = coord_complete, d= network_complete, directed = TRUE)
+
+V(tr_net)$color <- ifelse(names(V(tr_net)) < 18, "lightblue", "gray")
+
+plot.igraph(tr_net,layout=as.matrix(coord_complete[,2:3]),
+            vertex.color=adjustcolor("gray",alpha.f=0.5), vertex.size=3, vertex.label=NA,
+            link=adjustcolor("red",alpha.f=0.5), edge.width=1,
+            edge.arrow.size=.1)
 
 #real zones
 
 realz<- network$B_NODE[network$A_NODE %in% 1:17]
 names<-coord$NAME[1:17]
 coord$NAME[1:17]<-NA
-for (i in 1:17){
-coord$NAME[coord$NODE==realz[i]]<-names[i]}
+for (i in 1:18){
+  coord$NAME[coord$NODE==realz[i]]<-names[i]}
 
-tr_net<-graph_from_data_frame(as.matrix(network), directed = TRUE, vertices=coord)
+E(tr_net)$TIME_min <-  E(tr_net)$DIST / E(tr_net)$SPEED_A_B / 60
+E(tr_net)$LOAD <- 0
 
-tr_net<-graph.data.frame(as.matrix(network[, .(A_NODE, B_NODE)]), directed = FALSE)
+#prova con zone originali
 
-plot.igraph(tr_net,layout=as.matrix(coord[,2:3]),
-            vertex.color=adjustcolor("gray",alpha.f=0.5), vertex.size=3, vertex.label=NA,
-            link=adjustcolor("red",alpha.f=0.5), edge.width=3,
-            edge.arrow.size=4)
+short_path <- shortest_paths(tr_net, matOD[VEH_H>0,]$ZONE_O, matOD[VEH_H>0,]$ZONE_D, mode = 'out',
+               weights = E(tr_net)$TIME_min, output = "both",
+               predecessors = FALSE, inbound.edges = FALSE)
 
+#fare loop sulle shortpath aumentanto E attr load con il numero di veicoli assegnati
+for (i in short_path$epath){print(i)}
 
+#ricalcolare cost/speed ?
+# capire cap index
